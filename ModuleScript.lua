f07ed1a8978a55b8517b65ff9a81b7bd52140381
fruitmonkey01 <ericfeng35@yahoo.com>
2022-04-14 23:41:23 -0700

    Updated Lua Module Script for Planet Rotation
    Added support for each planets to rotate at the same time.

--- ModuleScript.lua
+++ ModuleScript.lua
@@ -1,19 +1,27 @@
 local module = {}
 
-function module.rotate(planet)
+local RunService = game:GetService("RunService")
+local incr = 360/10
+local deg = 0
 
+function module.rotate(planet, rotX, rotY, rotZ)
 	print(planet.Name)
-
 	-- for orientation
-	local incr = 360/20
-
-	while true do
-		for deg = 0, 360, incr do
+	RunService.RenderStepped:Connect(function()
+		deg = deg + incr
+		if rotX then
+			planet.Rotation = Vector3.new(deg, 0, 0)
+		elseif rotY then
 			planet.Rotation = Vector3.new(0, deg, 0)
-			print(deg)
-			wait(1)
+		else
+			planet.Rotation = Vector3.new(0, 0, deg)
 		end
-	end
+		print(deg)
+		if deg >= 360 then
+			deg = 0
+		end
+	end)
+	
 end
 
 return module
--- PlanetModel.lua
+++ PlanetModel.lua
@@ -11,6 +11,7 @@ print("Hello world!")
 local model = Instance.new("Model")
 model.Name = "Model1"
 model.Parent = game.Workspace
+local stud_offset = 8
 
 -- creating the basic planet
 local planet1 = Instance.new("Part")
@@ -24,22 +25,26 @@ planet1.CanCollide = true
 -- assign the object's Parent to the Model
 planet1.Parent = model
 planet1.Name = "Planet1"
+planet1.Color = Color3.new(0.551961, 0.55095, 0.8500404)
 
 -- creating the 2nd cloned planet with Position offset added 8 studs on the X and Z coordinate
 local planet2 = planet1:Clone()
-planet2.Position = Vector3.new(planet1.Position.X + 8, planet1.Position.Y, planet1.Position.Z + 8)
+planet2.Position = Vector3.new(planet1.Position.X + stud_offset, planet1.Position.Y, planet1.Position.Z + stud_offset)
 planet2.Parent = model
 planet2.Name = "Planet2"
+planet2.Color = Color3.new(0.501961, 0.25095, 0.0100404) -- Moca
 
 -- creating the 3rd cloned planet with Position offset minused 8 studs on the X and Z coordinate
 local planet3 = planet1:Clone()
-planet3.Position = Vector3.new(planet1.Position.X - 8, planet1.Position.Y, planet1.Position.Z - 8)
+planet3.Position = Vector3.new(planet1.Position.X - stud_offset, planet1.Position.Y, planet1.Position.Z - stud_offset)
 planet3.Parent = model
 planet3.Name = "Planet3"
+planet3.Color = Color3.new(0.5, 1, 0.5) -- Banana
 
 -- call rotate function for orientation, which defined in the ModuleScript under the game.ReplicatedStorage
 local module = require(game.ReplicatedStorage.ModuleScript)
+
 -- pass the part to rotate itself, for example: planet2
-module.rotate(planet2)
-
-
+module.rotate(planet1, true, false, false)
+module.rotate(planet2, false, true, false)
+module.rotate(planet3, false, false, true)
